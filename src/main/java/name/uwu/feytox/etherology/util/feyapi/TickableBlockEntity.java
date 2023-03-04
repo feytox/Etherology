d69ae85033ab7b3e3c9e1ad55ab73b023f6ca4cb
Feytox <feytox@vk.com>
2023-03-04 11:39:10 +0500

    [A] abstract TickableBlockEntity
    Took 1 hour 1 minute

---
+++ src/main/java/name/uwu/feytox/etherology/util/feyapi/TickableBlockEntity.java
@@ -0,0 +1,30 @@
+package name.uwu.feytox.etherology.util.feyapi;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public abstract class TickableBlockEntity extends BlockEntity {
+    public TickableBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    public abstract void clientTick(ClientWorld world, BlockPos blockPos, BlockState state);
+    public abstract void serverTick(ServerWorld world, BlockPos blockPos, BlockState state);
+
+    public static void clientTicker(World world, BlockPos blockPos, BlockState state, BlockEntity blockEntity) {
+        if (blockEntity instanceof TickableBlockEntity be) {
+            be.clientTick((ClientWorld) world, blockPos, state);
+        }
+    }
+
+    public static void serverTicker(World world, BlockPos blockPos, BlockState state, BlockEntity blockEntity) {
+        if (blockEntity instanceof TickableBlockEntity be) {
+            be.serverTick((ServerWorld) world, blockPos, state);
+        }
+    }
+}
