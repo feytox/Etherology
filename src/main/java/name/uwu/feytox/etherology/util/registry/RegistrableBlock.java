bdd8f9d188cfa2aac2ad5622d190d61906567da3
Feytox <feytox@vk.com>
2023-03-06 17:46:48 +0500

    [A] RegistrableBlock interface
    Took 59 minutes

---
+++ src/main/java/name/uwu/feytox/etherology/util/registry/RegistrableBlock.java
@@ -0,0 +1,33 @@
+package name.uwu.feytox.etherology.util.registry;
+
+import name.uwu.feytox.etherology.util.feyapi.EIdentifier;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
+public interface RegistrableBlock {
+    Block getBlockInstance();
+    String getBlockId();
+
+    default Block registerAll() {
+        Block block = registerBlock();
+        registerItem();
+        return block;
+    }
+
+    default Block registerBlock() {
+        Block block = getBlockInstance();
+        String blockId = getBlockId();
+        return Registry.register(Registries.BLOCK, new EIdentifier(blockId), block);
+    }
+
+    default BlockItem registerItem() {
+        Block block = getBlockInstance();
+        String blockId = getBlockId();
+        // TODO: 06/03/2023 насчёт настроек подумать
+        BlockItem blockItem = new BlockItem(block, new FabricItemSettings());
+        return Registry.register(Registries.ITEM, new EIdentifier(blockId), blockItem);
+    }
+}
