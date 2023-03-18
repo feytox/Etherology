ff4fdd967732577b5162013aeb2a79427c9ed0d7
Feytox <feytox@vk.com>
2023-03-18 17:35:22 +0500

    [C] glint bundle interface for ether count
    Took 55 minutes

--- src/main/java/ru/feytox/etherology/items/glints/AbstractGlintItem.java
+++ src/main/java/ru/feytox/etherology/items/glints/AbstractGlintItem.java
@@ -1,11 +1,24 @@
 package ru.feytox.etherology.items.glints;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.item.BundleTooltipData;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.item.TooltipData;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import ru.feytox.etherology.util.feyapi.NoDamageHandler;
 import ru.feytox.etherology.util.registry.SimpleItem;
+
+import java.util.List;
+import java.util.Optional;
+
+import static ru.feytox.etherology.ItemsRegistry.ETHER_SHARD;
 
 public abstract class AbstractGlintItem extends SimpleItem {
     private final float maxEther;
@@ -23,6 +36,37 @@ public abstract class AbstractGlintItem extends SimpleItem {
         stack.setNbt(nbt);
 
         return stack;
+    }
+
+    @Override
+    public Optional<TooltipData> getTooltipData(ItemStack stack) {
+        NbtCompound nbt = stack.getNbt();
+        if (nbt == null) return Optional.empty();
+
+        float storedEther = nbt.getFloat("stored_ether");
+        int etherValue = MathHelper.floor(storedEther);
+
+        DefaultedList<ItemStack> defaultedList = DefaultedList.of();
+        for (int i = 0; i < 3 && etherValue > 0; i++) {
+            int count = Math.min(64, etherValue);
+            etherValue -= count;
+            ItemStack etherStack = ETHER_SHARD.getDefaultStack();
+            etherStack.setCount(count);
+            defaultedList.add(etherStack);
+        }
+
+        return Optional.of(new BundleTooltipData(defaultedList, MathHelper.floor(storedEther)));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        NbtCompound nbt = stack.getNbt();
+        if (nbt == null) return;
+
+        float storedEther = nbt.getFloat("stored_ether");
+        int etherValue = MathHelper.floor(storedEther);
+
+        tooltip.add(Text.translatable("item.etherology.glint.fullness", etherValue, MathHelper.floor(getMaxEther())).formatted(Formatting.GRAY));
     }
 
     public float getMaxEther() {
