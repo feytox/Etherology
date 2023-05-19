7ec2c8771dc25f627bebaba1c9b2a62017569f03
Feytox <feytox@vk.com>
2023-05-19 18:09:49 +0500

    [A] Hammer attack raw impl
    Took 52 minutes

--- src/main/java/ru/feytox/etherology/mixin/PlayerEntityMixin.java
+++ src/main/java/ru/feytox/etherology/mixin/PlayerEntityMixin.java
@@ -2,18 +2,25 @@ package ru.feytox.etherology.mixin;
 
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import ru.feytox.etherology.item.HammerItem;
 import ru.feytox.etherology.util.feyapi.PlayerAnimations;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Mixin(PlayerEntity.class)
@@ -41,5 +48,35 @@ public class PlayerEntityMixin {
         PlayerEntity it = ((PlayerEntity) (Object) this);
         if (!(it instanceof AbstractClientPlayerEntity clientPlayer)) return;
         CompletableFuture.runAsync(() -> PlayerAnimations.tickAnimations(clientPlayer));
+    }
+
+    @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;takeKnockback(DDD)V"))
+    private void vanillaKnockbackCancel(LivingEntity instance, double strength, double x, double z) {
+        PlayerEntity attacker = ((PlayerEntity) (Object) this);
+        if (attacker.getMainHandStack().getItem() instanceof HammerItem && attacker.getOffHandStack().isEmpty()) return;
+        instance.takeKnockback(strength, x, z);
+    }
+
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @Inject(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;takeKnockback(DDD)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void onHammerKnockback(Entity target, CallbackInfo ci, float f, float g, boolean bl, boolean bl2, int i, boolean bl3, boolean bl4, float j, boolean bl5, int k, Vec3d vec3d, float l, List<LivingEntity> list, Iterator<LivingEntity> var19, LivingEntity livingEntity) {
+        PlayerEntity attacker = ((PlayerEntity) (Object) this);
+        if (!(attacker.getMainHandStack().getItem() instanceof HammerItem hammer) || !attacker.getOffHandStack().isEmpty()) return;
+        Vec3d hammerVec = livingEntity.getPos().subtract(target.getPos());
+        float radius = 2f * hammer.getAttackDamage() * l;
+        float distance = livingEntity.distanceTo(target);
+        if (distance > radius) return;
+        hammerVec = hammerVec.multiply(2 * radius / distance);
+        livingEntity.takeKnockback(1, hammerVec.x, hammerVec.z);
+    }
+
+    @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Box;expand(DDD)Lnet/minecraft/util/math/Box;"))
+    private Box expandHammerAttack(Box instance, double x, double y, double z) {
+        PlayerEntity attacker = ((PlayerEntity) (Object) this);
+        if (!(attacker.getMainHandStack().getItem() instanceof HammerItem) || !attacker.getOffHandStack().isEmpty()) {
+            return instance.expand(x, y, z);
+        }
+
+        return instance.expand(x+4, y, z+4);
     }
 }
