98f2da62ded7ac3a15a941507dd59aa9244383b1
Feytox <feytox@vk.com>
2024-05-10 20:57:32 +0500

    feature: ebony armor speed modifier
    Took 45 minutes

---
+++ src/main/java/ru/feytox/etherology/mixin/ArmorItemMixin.java
@@ -0,0 +1,33 @@
+package ru.feytox.etherology.mixin;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.feytox.etherology.registry.util.EtherArmorMaterials;
+
+import java.util.UUID;
+
+@Mixin(ArmorItem.class)
+public class ArmorItemMixin {
+
+    @Shadow @Final private static UUID[] MODIFIERS;
+
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMultimap$Builder;build()Lcom/google/common/collect/ImmutableMultimap;"))
+    private void injectEbonySpeedBoost(ArmorMaterial material, EquipmentSlot slot, Item.Settings settings, CallbackInfo ci, @Local ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder) {
+        if (!material.equals(EtherArmorMaterials.EBONY)) return;
+        UUID uUID = MODIFIERS[slot.getEntitySlotId()];
+        builder.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(uUID, "Armor movement speed", 0.075d, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
+    }
+}
