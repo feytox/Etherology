9f0274635a1defa64d77d94e1e8fac0884c9dd78
Feytox <73577753+feytox@users.noreply.github.com>
2024-11-03 21:38:29 +0500

    nerf: crumb nutrition 5 -> 3
    Took 3 minutes

--- src/main/java/ru/feytox/etherology/registry/item/EFoodComponents.java
+++ src/main/java/ru/feytox/etherology/registry/item/EFoodComponents.java
@@ -6,5 +6,5 @@ import net.minecraft.component.type.FoodComponent;
 @UtilityClass
 public class EFoodComponents {
 
-    public static final FoodComponent CRUMB = new FoodComponent.Builder().nutrition(5).saturationModifier(6).build();
+    public static final FoodComponent CRUMB = new FoodComponent.Builder().nutrition(3).saturationModifier(6).build();
 }
